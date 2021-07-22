fbeddb94569588970653c3757d50732cbf9bb9ac
Eva Sibinga <evasibinga@MacBook-Pro-5.local>
2021-07-22 10:44:07 -0400

    added contour for gradient filled shape

--- gradients.js
+++ gradients.js
@@ -15,21 +15,22 @@ function setup() {
     button.position(50, 30);
     button.mousePressed(setGradient);
 
-    button = createButton('change shapes');
+    button = createButton('add shapes');
     button.position(50, 60);
     button.mousePressed(setShape);
 
 }
 
-// function draw() {
-//     ellipse(mouseX, mouseY, 30, 30);
-// }
+function draw() {
+    //ellipse(mouseX, mouseY, 30, 30);
+    //  console.log("x y is ", mouseX, mouseY)
+}
 
 
 // DONE: extrapolate this to a function / for loop to generate as many quads as i want
 
 function setGradient() { //create color background
-
+    clear();
     //color randomness
     let r1 = random(256)
     let g1 = random(256)
@@ -40,16 +41,14 @@ function setGradient() { //create color background
     let opacity1 = random();
     let opacity2 = random();
 
-
     let c1 = color(r1, g1, b1, random(128));
     let c2 = color(r2, g2, b2, random(128));
     // noprotect
 
     noFill();
-
     for (var x = 0; x < width; x += width / 3) {
-        for (var y = 0; y < 250; y++) {
-            var inter = map(y, 0, 250, 0, 1);
+        for (var y = 0; y < 230; y++) {
+            var inter = map(y, 0, 230, 0, 1);
             var range = lerpColor(c1, c2, inter);
             var reverseRange = lerpColor(c2, c1, inter);
 
@@ -76,34 +75,70 @@ function setShape() {
             stroke(000);
             strokeWeight(2);
 
-            erase(100, 255);
-
-            // make shape
+            // make shape - rectangle surrounding blob
             beginShape();
+            vertex(x, 0);
+            vertex(200 + x, 0);
+            vertex(200 + x, 0);
+            vertex(200 + x, 250);
+            vertex(200 + x, 250);
+            vertex(x, 250);
+            vertex(x, 250);
+
+            erase(255, 255);
+
+            // make blob - contour
+            beginContour();
             // N
-            curveVertex(70 + x, 150 + y); // determines curvature
+            // curveVertex(70 + x, 150 + y); // determines curvature
+            curveVertex(0 + x, 0 + y);
             curveVertex(70 + x, 50 + y);
-            // NE
-            curveVertex(120 + i + x, 60 + j + y);
-            // E
-            curveVertex(170 + x, 105 + y);
-            // SE
-            curveVertex(125 + i + x, 165 + j + y);
-            // S
-            curveVertex(90 + x, 200 + y);
-            // SW
-            curveVertex(55 + i + x, 165 + j + y);
+            //NW
+            curveVertex(50 + i + x, 100 + j + y);
             // W
             curveVertex(45 + x, 125 + y);
-            //NW
-            curveVertex(40 + i + x, 100 + j + y);
+            // SW
+            curveVertex(55 + i + x, 165 + j + y);
+            // S
+            curveVertex(90 + x, 200 + y);
+            // SE
+            curveVertex(125 + i + x, 165 + j + y);
+            // E
+            curveVertex(170 + x, 105 + y);
+            // NE
+            curveVertex(120 + i + x, 60 + j + y);
             // end (N)
-            curveVertex(80 + x, 50 + y);
-            curveVertex(300 + x, 150 + y); // determines curvature
-            endShape();
+            // curveVertex(80 + x, 50 + y);
+            // curveVertex(70 + x, 150 + y); // determines curvature
+            curveVertex(70 + x, 50 + y);
+            curveVertex(70 + x, 50 + y);
+            //curveVertex(300 + x, 150 + y); // determines curvature
+            endContour();
+
+            endShape(CLOSE);
 
             noErase();
         }
     }
 
 }
+
+/*
+        // N
+        curveVertex(70 + x, 150 + y); // determines curvature
+        curveVertex(70 + x, 50 + y);
+        // NE
+        curveVertex(120 + i + x, 60 + j + y);
+        // E
+        curveVertex(170 + x, 105 + y);
+        // SE
+        curveVertex(125 + i + x, 165 + j + y);
+        // S
+        curveVertex(90 + x, 200 + y);
+        // SW
+        curveVertex(55 + i + x, 165 + j + y);
+        // W
+        curveVertex(45 + x, 125 + y);
+        //NW
+        curveVertex(40 + i + x, 100 + j + y);
+        */
