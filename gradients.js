9174b74928d4cfa35de8a54d7afc080310466c7b
Eva Sibinga <evasibinga@MacBook-Pro-5.local>
2021-07-28 11:20:16 -0400

    added rgb sliders

--- gradients.js
+++ gradients.js
@@ -1,29 +1,69 @@
 
 var c1, c2;
-let button;
+//make these global so they can be accessed from any function
+let buttonColor;
+let buttonShape;
+let slider1;
+let slider2;
+let slider3;
+let slider4;
+let slider5;
+let slider6;
+let r1;
+let g1;
+let b1;
+let r2;
+let g2;
+let b2;
 
 function setup() {
     createCanvas(1200, 500); //(x, y)
 
     // run the first gradient
     setGradient(c1, c2);
-
     setShape();
 
     //make a button that runs setGradient when clicked
-    button = createButton('change colors');
-    button.position(50, 30);
-    button.mousePressed(setGradient);
+    buttonColor = createButton('change colors');
+    buttonColor.position(50, 30);
+    buttonColor.mousePressed(setGradient);
 
-    button = createButton('add shapes');
-    button.position(50, 60);
-    button.mousePressed(setShape);
+    buttonShape = createButton('add shapes');
+    buttonShape.position(50, 60);
+    buttonShape.mousePressed(setShape);
 
+
+    // make a slider
+    // createSlider(min, max, startingValue)
+    slider1 = createSlider(0, 256, r1)
+    slider2 = createSlider(0, 256, g1)
+    slider3 = createSlider(0, 256, b1)
+    slider4 = createSlider(0, 256, r2)
+    slider5 = createSlider(0, 256, g2)
+    slider6 = createSlider(0, 256, b2)
+    slider1.position(250, 30);
+    slider2.position(250, 50);
+    slider3.position(250, 70);
+    slider4.position(450, 30);
+    slider5.position(450, 50);
+    slider6.position(450, 70);
+
+    console.log("slider value", slider1.value())
+
+    slider1.input(setGradient);
+    slider2.input(setGradient);
+    slider3.input(setGradient);
+    slider4.input(setGradient);
+    slider5.input(setGradient);
+    slider6.input(setGradient);
+
+    slider1.style('background-color', '#ff0000');
 }
 
 function draw() {
     //ellipse(mouseX, mouseY, 30, 30);
     // console.log("x y is ", mouseX, mouseY)
+
 }
 
 
@@ -31,18 +71,18 @@ function draw() {
 
 function setGradient() { //create color background
     clear();
-    //color randomness
-    let r1 = random(256)
-    let g1 = random(256)
-    let b1 = random(256)
-    let r2 = random(256)
-    let g2 = random(256)
-    let b2 = random(256)
-    let opacity1 = random();
-    let opacity2 = random();
 
-    let c1 = color(r1, g1, b1, random(128));
-    let c2 = color(r2, g2, b2, random(128));
+    //set color values using sliders
+    if (slider1) { r1 = slider1.value() } else { r1 = random(256); }
+    if (slider2) { g1 = slider2.value() } else { g1 = random(256); }
+    if (slider3) { b1 = slider3.value() } else { b1 = random(256); }
+    if (slider4) { r2 = slider4.value() } else { r2 = random(256); }
+    if (slider5) { g2 = slider5.value() } else { g2 = random(256); }
+    if (slider6) { b2 = slider6.value() } else { b2 = random(256); }
+
+    console.log(r1)
+    let c1 = color(r1, g1, b1, 128); //can add opacity as a 4th value (incl random(128))
+    let c2 = color(r2, g2, b2, 128);
     // noprotect
 
     noFill();
@@ -85,7 +125,6 @@ function setShape() {
             vertex(x, 250);
             vertex(x, 250);
 
-            // erase(200, 255);
             erase(255, 255);
 
             // pretty sure you can't even really do a blob contour so this is truly just a mess
@@ -113,11 +152,9 @@ function setShape() {
             curveVertex(70 + x, 50 + y);
             curveVertex(30 + x, 150 + y);
             endContour(CLOSE);
-
             endShape(CLOSE);
 
             noErase();
         }
     }
-
 }
